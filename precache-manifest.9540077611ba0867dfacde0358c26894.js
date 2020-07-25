bc7eec6fcab360d6c77149275a4083da39019f37
owner <owner@ownernoMacBook-puro.local>
2020-07-25 14:25:11 +0900

    Updates

---
+++ precache-manifest.9540077611ba0867dfacde0358c26894.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc96e37fc7ef440036c016d27a8f245",
+    "url": "/taguchi-takahiro/chatbot.git/index.html"
+  },
+  {
+    "revision": "f520169e5ddffcfd7a38",
+    "url": "/taguchi-takahiro/chatbot.git/static/css/main.50841beb.chunk.css"
+  },
+  {
+    "revision": "acc53e476917fe228d76",
+    "url": "/taguchi-takahiro/chatbot.git/static/js/2.2f099d4a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/taguchi-takahiro/chatbot.git/static/js/2.2f099d4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f520169e5ddffcfd7a38",
+    "url": "/taguchi-takahiro/chatbot.git/static/js/main.f43f120f.chunk.js"
+  },
+  {
+    "revision": "ca5c683e3ff0c8fe1a47",
+    "url": "/taguchi-takahiro/chatbot.git/static/js/runtime-main.88a3299f.js"
+  },
+  {
+    "revision": "a990286c44c78576287e84609771f393",
+    "url": "/taguchi-takahiro/chatbot.git/static/media/no-profile.a990286c.png"
+  }
+]);
